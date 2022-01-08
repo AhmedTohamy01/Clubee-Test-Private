3865b0448ad6033061e204322ea131cd736126db
Ahmed Tohamy <ahmed-tohamy@outlook.com>
2022-01-09 00:06:20 +0200

    feat: return back original searchbox

--- src/Components/SearchBox/SearchBox.tsx
+++ src/Components/SearchBox/SearchBox.tsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
+import { CloseCircle } from '@styled-icons/ionicons-outline'
 import { PropsType } from './SearchBox.interfaces'
-import { TextField } from '@material-ui/core'
 
 /*---> Components <---*/
 const SearchBox = ({ searchTerm, setSearchTerm, filterTerm }: PropsType) => {
@@ -9,15 +9,18 @@ const SearchBox = ({ searchTerm, setSearchTerm, filterTerm }: PropsType) => {
       <Input
         data-cy='search-box'
         id='search-box'
+        placeholder='Search ...'
+        autoComplete='off'
         defaultValue={filterTerm}
         value={searchTerm || ''}
         onChange={(event) => setSearchTerm(event.target.value)}
-        variant='outlined'
-        label='Search here ...'
-        size='medium'
-        type='search'
-        color='primary'
-        autoComplete='off'
+      />
+      <CloseIcon
+        searchTerm={searchTerm}
+        onClick={() => {
+          setSearchTerm('')
+          window.location.reload()
+        }}
       />
     </SearchBoxWrapper>
   )
@@ -29,9 +32,28 @@ const SearchBoxWrapper = styled.div`
   align-items: center;
 `
 
-const Input = styled(TextField)`
+const Input = styled.input`
+  border: 1px solid rgba(0, 0, 0, 0.1);
+  border-radius: 8px;
   width: 400px;
-  background-color: white;
+  height: 64px;
+  padding: 16px;
+  font-size: 24px;
+  font-weight: normal;
+
+  ::placeholder {
+    font-size: 24px;
+    line-height: 30px;
+  }
+`
+
+const CloseIcon = styled(CloseCircle)<{ searchTerm: string }>`
+  margin-left: -35px;
+  width: 30px;
+  height: 30px;
+  cursor: pointer;
+  color: #979797;
+  display: ${(props) => (props.searchTerm ? 'initial' : 'none')};
 `
 
 export default SearchBox
